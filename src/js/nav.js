9b841d45c60d3e70067daae161a18a711fbd9029
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-08-17 16:05:06 +0100

    Fix nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,8 +1,9 @@
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
     var navbar = document.getElementById("nav");
+    var sticky = navbar.offsetTop;
 
-    if (window.scrollY >= navbar.offsetTop) {
+    if (window.scrollY >= sticky) {
         navbar.classList.add("nav--stick")
     } else {
         navbar.classList.remove("nav--stick");
