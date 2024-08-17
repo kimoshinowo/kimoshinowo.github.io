cc583dc4d195e3ff57cbbf352d24dabf05577b74
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-08-17 16:58:38 +0100

    Add smooth scroll js

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,6 @@
 // When the user scrolls the page, execute myFunction
 var navbar = document.getElementById("nav");
-const sticky = navbar.offsetTop;
+var sticky = navbar.offsetTop;
 
 window.onscroll = function() {
     if (window.scrollY >= sticky) {
@@ -9,3 +9,22 @@ window.onscroll = function() {
         navbar.classList.remove("nav--stick");
     }
 };
+
+var aboutSection = document.getElementById("about");
+var projectsSection = document.getElementById("projects");
+var contactSection = document.getElementById("contact");
+var aboutSectionPos = aboutSection.offsetTop;
+var projectsSectionPos = aboutSection.offsetTop;
+var contactSectionPos = aboutSection.offsetTop;
+
+function smoothScrollWithOffset(target) {
+    if (target == 'about') {
+        var pos = aboutSectionPos;
+    } else if (target == 'projects') {
+        var pos = projectsSectionPos;
+    } else if (target == 'contact') {
+        var pos = contactSectionPos;
+    }
+
+    window.scrollTo(pos + 60);
+}
