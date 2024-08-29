f26194acfdc18f81f6df209b7d4adcd15b231817
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-08-29 15:32:45 +0100

    Run func for each shape

--- src/js/home-animation.js
+++ src/js/home-animation.js
@@ -1,17 +1,22 @@
-var id = null;
-
-var elem = document.getElementById("shape");
 var screenHeight = document.getElementById("homescreen").offsetHeight;
 var screenWidth = document.getElementById("homescreen").offsetWidth;
 
-var yPos = getRndInteger(1, screenHeight);
-var xPos = getRndInteger(1, screenWidth);
 
-yDir = Math.round(Math.random()) * 2 - 1;
-xDir = Math.round(Math.random()) * 2 - 1;
+for (let i = 1; i < 4; i++) {
+    var id = null;
 
-clearInterval(id);
-id = setInterval(frame, 10);
+    let shape = "shape"
+    var elem = document.getElementById(shape.concat(i.toString()));
+    var yPos = getRndInteger(1, screenHeight);
+    var xPos = getRndInteger(1, screenWidth);
+
+    yDir = Math.round(Math.random()) * 2 - 1;
+    xDir = Math.round(Math.random()) * 2 - 1;
+
+    clearInterval(id);
+    id = setInterval(frame, 10);
+}
+
 
 function frame() {
     if (((yPos + 200) >= screenHeight) || (yPos <= 0)) {
