0c9f497997e8390de5befa3864d27285e2206f2b
kimoshinowo <kimoshinowo@hotmail.co.uk>
2024-08-29 14:21:29 +0100

    remove clear interval and edit start point

--- src/js/home-animation.js
+++ src/js/home-animation.js
@@ -4,17 +4,16 @@ var elem = document.getElementById("shape");
 var screenHeight = document.getElementById("homescreen").offsetHeight;
 var screenWidth = document.getElementById("homescreen").offsetWidth;
 
-var yPos = 0;
-var xPos = 0;
+var yPos = -200;
+var xPos = -200;
 
 clearInterval(id);
 id = setInterval(frame, 10);
 
 function frame() {
     if ((yPos == screenHeight) || (xPos == screenWidth)) {
-        clearInterval(id);
-        yPos = 0;
-        xPos = 0;
+        yPos = -200;
+        xPos = -200;
     } else {
         yPos++;
         xPos++;
